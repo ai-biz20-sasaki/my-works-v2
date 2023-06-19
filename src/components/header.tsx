8c57b021c97392a3f5bad9c4a6771464ac5338d8
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-19 09:03:33 +0900

    コンポーネントを追加、ルートページの差し替え

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">ポートフォリオ@aibizsasaki</h1>
+    </div>    
+  )
+}
---
+++ src/components/right-sidebar.tsx
@@ -0,0 +1,7 @@
+export default function RightSidebar() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">プロフィール</h1>
+    </div>    
+  )
+}
