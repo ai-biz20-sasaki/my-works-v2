b69007f50250e4df3366904d132b670a373b110b
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-20 13:59:11 +0900

    作品のurlをクリック時、別ウィンドウで開くように修正

--- src/components/left-sideber.tsx
+++ src/components/left-sideber.tsx
@@ -6,36 +6,48 @@ export default function LeftSidebar() {
       </h1>
 
       <h1 className="text-center border-2 border-indigo-500/30 p-3 my-2">
-        <a className="text-2xl hover:text-indigo-300"
-          href="https://whiteboard.aislaboratory.com/">
-            Whiteboard
+        <a 
+          className="text-2xl hover:text-indigo-300"
+          href="https://whiteboard.aislaboratory.com/"
+          target="_blank" rel="noopener noreferrer"
+        >
+          Whiteboard
         </a>
         <p>手書きの図を描画するWebサイト</p>
         <p>Next.js/TypeScript/tailwind</p>
       </h1>
 
       <h1 className="text-center border-2 border-indigo-500/30 p-3 my-2">
-        <a className="text-2xl hover:text-indigo-300"
-          href="https://videolibrary.0gravity0.com/">
-            Video Libraly
+        <a 
+          className="text-2xl hover:text-indigo-300"
+          href="https://videolibrary.0gravity0.com/"
+          target="_blank" rel="noopener noreferrer"
+        >
+          Video Libraly
         </a>
         <p>Amazon Prime Videoをスクレイピングして表示するWebサイト</p>
         <p>Laravel/PHP+MySQL</p>
       </h1>
 
       <h1 className="text-center border-2 border-indigo-500/30 p-3 my-2">
-        <a className="text-2xl hover:text-indigo-300"
-          href="https://kabuboard2.0gravity0.com/">
-            Kabuboard
+        <a
+          className="text-2xl hover:text-indigo-300"
+          href="https://kabuboard2.0gravity0.com/"
+          target="_blank" rel="noopener noreferrer"
+        >
+          Kabuboard
         </a>
         <p>Yahooファイナンスをスクレイピングし株価を表示などするWebサイト</p>
         <p>Laravel/PHP+MySQL</p>
       </h1>
 
       <h1 className="text-center border-2 border-indigo-500/30 p-3 my-2">
-        <a className="text-2xl hover:text-indigo-300"
-          href="https://resume-0gravity.an.r.appspot.com/">
-            Resume
+        <a 
+          className="text-2xl hover:text-indigo-300"
+          href="https://resume-0gravity.an.r.appspot.com/"
+          target="_blank" rel="noopener noreferrer"
+        >
+          Resume
         </a>
         <p>履歴書の作成支援Webサイト</p>
         <p>vue.js/JavaScript+Flask/python+GCP(GAE+Datastore)</p>
